e18a595758eeedd1fc28a35becabe8f4f67bf884
rsiyun <rsiyun@gmail.com>
2021-07-18 22:29:25 +0700

    Android-PBO 5-10
    Android-PBO 5-10

---
+++ kalkulator/app/src/main/java/com/rsiyun/kalkulator/Kalkulator.java
@@ -0,0 +1,17 @@
+package com.rsiyun.kalkulator;
+
+public class Kalkulator {
+    public double penjumlahan(double a, double b){
+    return a+b;
+    }
+    public double pengurangan(double a, double b){
+        return a-b;
+    }
+    public double perkalian(double a, double b){
+        return a*b;
+    }
+    public double pembagian(double a, double b){
+        return a/b;
+    }
+
+}
--- kalkulator/app/src/main/java/com/rsiyun/kalkulator/MainActivity.java
+++ kalkulator/app/src/main/java/com/rsiyun/kalkulator/MainActivity.java
@@ -2,12 +2,38 @@ package com.rsiyun.kalkulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText bil_1,bil_2;
+    TextView hasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hitung(View view) {
+        bil_1 = findViewById(R.id.etBil_1);
+        bil_2 = findViewById(R.id.etBil_2);
+        hasil = findViewById(R.id.tvHasil);
+        if (bil_1.getText().toString().equals("")){
+            bil_1.setError("Harus di isi");
+        }
+        else if(bil_2.getText().toString().equals("")){
+            bil_2.setError("Harus di isi");
+        }
+        else{
+            double a = Double.parseDouble(bil_1.getText().toString());
+            double b = Double.parseDouble(bil_2.getText().toString());
+            Kalkulator calc = new Kalkulator();
+
+            double c = calc.pengurangan(a,b);
+            hasil.setText(c + "");
+        }
     }
 }
